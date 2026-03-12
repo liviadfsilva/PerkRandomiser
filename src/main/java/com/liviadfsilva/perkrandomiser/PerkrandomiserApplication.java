d6f1c95f6b39848672194fbdeaed4c4387636cde
liviadfsilva <liviadfsilva@gmail.com>
2026-03-12 12:54:14 -0300

    first commit

---
+++ src/main/java/com/liviadfsilva/perkrandomiser/PerkrandomiserApplication.java
@@ -0,0 +1,13 @@
+package com.liviadfsilva.perkrandomiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerkrandomiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerkrandomiserApplication.class, args);
+	}
+
+}
