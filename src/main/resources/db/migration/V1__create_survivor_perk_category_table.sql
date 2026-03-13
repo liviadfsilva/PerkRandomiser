046566d696c6a8e395e26e3507e998f704e0019f
liviadfsilva <liviadfsilva@gmail.com>
2026-03-13 17:22:37 -0300

    feat: create SurvivorPerkCategory model and migrations

---
+++ src/main/resources/db/migration/V1__create_survivor_perk_category_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE survivor_perk_category(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__insert_survivor_perk_categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO survivor_perk_category (name)
+VALUES
+('Aura Reading'),
+('Chase'),
+('Co-Op'),
+('Endgame'),
+('Generator'),
+('Healing'),
+('Killer Stun'),
+('Totem');
