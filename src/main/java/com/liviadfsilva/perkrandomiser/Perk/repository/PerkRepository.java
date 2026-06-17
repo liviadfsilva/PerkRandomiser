936c5949405c3a47659bff076da57ee21509ea79
liviadfsilva <liviadfsilva@gmail.com>
2026-06-17 08:50:20 -0300

    feat: create PerkRepository

---
+++ src/main/java/com/liviadfsilva/perkrandomiser/Perk/repository/PerkRepository.java
@@ -0,0 +1,7 @@
+package com.liviadfsilva.perkrandomiser.Perk.repository;
+
+import com.liviadfsilva.perkrandomiser.Perk.model.Perk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerkRepository extends JpaRepository<Perk, Long> {
+}
