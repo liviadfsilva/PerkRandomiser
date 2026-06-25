87a0281e7ba0333d04860e05c2e97ef965f74c8b
liviadfsilva <liviadfsilva@gmail.com>
2026-06-25 20:22:37 -0300

    feat: create user model

---
+++ src/main/java/com/liviadfsilva/perkrandomiser/User/model/User.java
@@ -0,0 +1,26 @@
+package com.liviadfsilva.perkrandomiser.User.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password_hash", nullable = false)
+    private String password;
+}
